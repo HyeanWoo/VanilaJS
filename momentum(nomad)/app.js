4d642d0f2ee40cd891e7336653bc00c86f8aace5
Hyeanwoo <wkdus7h@naver.com>
2021-08-16 22:52:21 +0900

    Add logout

--- momentum(nomad)/app.js
+++ momentum(nomad)/app.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutBtn = document.querySelector("#logout-btn");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "momentum_username";
@@ -17,13 +18,23 @@ function onLoginSubmit(e) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  logoutBtn.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function onLogout() {
+  localStorage.removeItem(USERNAME_KEY);
+  greeting.classList.add(HIDDEN_CLASSNAME);
+  logoutBtn.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+loginForm.addEventListener("submit", onLoginSubmit);
+logoutBtn.addEventListener("click", onLogout);
+
 if (!savedUsername) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
 }
