aa305cdd16858636e9d3ca5f2cf12b098b5077dd
Hyeanwoo <wkdus7h@naver.com>
2021-08-16 22:32:57 +0900

    Add localstorage save function

--- momentum(nomad)/app.js
+++ momentum(nomad)/app.js
@@ -1,16 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const usernameHead = document.querySelector("#greeting");
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "momentum_username";
 
 function onLoginSubmit(e) {
   e.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
   const username = loginInput.value;
-  usernameHead.innerText = `Hello ${username}!`;
-  usernameHead.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (!savedUsername) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
