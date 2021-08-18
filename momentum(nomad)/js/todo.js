d23e812c7c27e3d0f528f5ec9803a8c3f90184d9
Hyeanwoo <wkdus7h@naver.com>
2021-08-18 20:58:48 +0900

    Add delete todos

--- momentum(nomad)/js/todo.js
+++ momentum(nomad)/js/todo.js
@@ -2,15 +2,30 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "momentum_todos";
+
+let toDos = JSON.parse(localStorage.getItem(TODOS_KEY)) || [];
+
+if (toDos.length !== 0) {
+  toDos.forEach((todo) => paintToDo(todo));
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteTodo(e) {
-  const li = e.target.parentElement.remove();
+  const li = e.target.parentElement;
+  toDos = toDos.filter((todo) => todo.id !== Number(li.id));
+  saveToDos();
   li.remove();
 }
 
-function paintToDo(newTodo) {
+function paintToDo(newToDo) {
   const li = document.createElement("li");
+  li.id = newToDo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newToDo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
 
@@ -22,9 +37,14 @@ function paintToDo(newTodo) {
 
 function handleToDoSubmit(e) {
   e.preventDefault();
-  const newTodo = toDoInput.value;
+  const newToDo = {
+    id: Date.now(),
+    text: toDoInput.value,
+  };
   toDoInput.value = "";
-  paintToDo(newTodo);
+  toDos.push(newToDo);
+  paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
